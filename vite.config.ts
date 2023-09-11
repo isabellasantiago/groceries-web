9249e0c044bbfa06944d2cc62c30053f7a78b6d2
Isabella Santiago <isabella.-.bela@hotmail.com>
2023-09-11 18:37:49 -0300

    chore: configuring test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -7,6 +9,10 @@ export default defineConfig({
   plugins: [
     react()
   ],
+  test: {
+    globals: true,
+    environment: "jsdom"
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname,'./src')
